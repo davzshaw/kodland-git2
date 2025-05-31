78f3f3be4b8ae8102eefcf7e2b08b4f1590620c9
David Torres <119999924+davzshaw@users.noreply.github.com>
2025-05-30 19:02:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ def sumar_numeros():
      try:
           numero_1 = int(input("Introduce el primer número: "))
           numero_2 = int(input("Introduce el segundo número: "))
-          print("Resultado de la suma:", numero_1 + numero_2)
+          return numero_1 + numero_2
       except ValueError:
           print("Error: Por favor, introduce solo números.")
-sumar_numeros()
+
+resultado = sumar_numeros()
+if resultado is not None:
+     print("Resultado de la suma:", resultado)
