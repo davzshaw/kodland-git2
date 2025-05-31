e2227705d5733795b1faf87a247a89ec5fb4dca3
David Torres <119999924+davzshaw@users.noreply.github.com>
2025-05-30 19:01:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 try:
-     a = int(input("Introduce el primer número: "))
-     b = int(input("Introduce el segundo número: "))
-     print("Resultado de la suma:", a + b)
+     numero_1 = int(input("Introduce el primer número: "))
+     numero_2 = int(input("Introduce el segundo número: "))
+     print("Resultado de la suma:", numero_1 + numero_2)
  except ValueError:
      print("Error: Por favor, introduce solo números.")
