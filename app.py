10b2183faa8d44b79c3606b722de0072327a514c
David Torres <119999924+davzshaw@users.noreply.github.com>
2025-05-30 19:01:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
-a = int(input("Introduce el primer número: "))
-b = int(input("Introduce el segundo número: "))
-print("Resultado de la suma:", a + b)
+try:
+     a = int(input("Introduce el primer número: "))
+     b = int(input("Introduce el segundo número: "))
+     print("Resultado de la suma:", a + b)
+ except ValueError:
+     print("Error: Por favor, introduce solo números.")
